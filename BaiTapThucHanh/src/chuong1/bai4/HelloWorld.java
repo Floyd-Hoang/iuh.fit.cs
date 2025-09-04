8c2dedb5b0ca2ebf8533820e0622edd9d0f7349e
Floyd-Hoang <hoangtienphat2k6@gmail.com>
2025-09-04 17:22:42 +0700

    Bai 4 - 04/09

---
+++ BaiTapThucHanh/src/chuong1/bai4/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * @description: 
+ * @author: 
+ * @version: 1.0
+ * @created: 04-Sep-2025 5:18:15 PM
+ */
+
+package chuong1.bai4;
+
+/**
+ * 
+ */
+public class HelloWorld {
+	    public static void main(String[] args) {
+	        // Cách 1: Sử dụng System.out.print() - không xuống dòng
+	        System.out.print("Hello World!");
+
+	        // Cách 2: Sử dụng System.out.println() - có xuống dòng
+	        System.out.println("Hello World!");
+
+	        // Cách 3: Sử dụng System.out.printf() - định dạng như C++
+	        System.out.printf("Hello World!");
+	    }
+	
+
+
+}
