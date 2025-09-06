334ade2284b39f149906ac5311339eca0e4f83f9
Floyd-Hoang <hoangtienphat2k6@gmail.com>
2025-09-06 22:54:30 +0700

    Bai 11 - 06/09

---
+++ BaiTapThucHanh/src/chuong1/bai11/OddNumbers.java
@@ -0,0 +1,19 @@
+/**
+ * @description: 
+ * @author: 
+ * @version: 1.0
+ * @created: 06-Sep-2025 10:42:56 PM
+ */
+
+package chuong1.bai11;
+
+/**
+ * 
+ */
+public class OddNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
