c6bc9f526c6e25b14629c231069a0f04b25014c6
Floyd-Hoang <hoangtienphat2k6@gmail.com>
2025-09-15 18:04:33 +0700

    Bai 18 - 15/09

---
+++ BaiTapThucHanh/src/chuong1/bai18/boisocua3.java
@@ -0,0 +1,24 @@
+/**
+ * @description: 
+ * @author: 
+ * @version: 1.0
+ * @created: 15-Sep-2025 6:03:25 PM
+ */
+
+package chuong1.bai18;
+
+/**
+ * 
+ */
+public class boisocua3 {
+	public static void main(String[] args) {
+        System.out.println("Các bội số của 3 từ 300 đến 3 là:");
+        for (int i = 300; i >= 3; i--) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
+
+}
